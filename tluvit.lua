a9d5c6ba916c077bf48ef9073c0441a27e57ca8f
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2022-10-01 23:52:23 +0000

    Bump to v1.4

--- tluvit.lua
+++ tluvit.lua
@@ -1,21 +1,21 @@
 --[[lit-meta
   name = "UrNightmaree/tluvit"
-  version = "1.3.2"
+  version = "1.4"
   description = "A Teal (.tl) runner for the Luvit runtime"
   tags = { "luvit", "teal" }
   dependencies = {
-    'truemedian/import'
+    'truemedian/import',
+    'luvit/fs'
   }
   license = "MIT"
   author = "UrNightmaree"
   homepage = "https://github.com/UrNightmaree/tluvit"
 ]]
 
-_G.lua_require = require
+local fs = require 'fs'
+
 _G.require = require
 module, import = require('import')(module)
-
-local HOME = os.getenv'HOME'
 
 --[[
  A Teal (.tl) runner for Luvit runtime
@@ -25,12 +25,12 @@ local tluvit = {}
 --[[
 Where the module should get the "tl.lua"
 ]]
-tluvit.tlpath = ';'..HOME..'/.luarocks/share/lua/5.1/?.lua;'..HOME..'/.luarocks/share/lua/5.1/?/init.lua'
+tluvit.tlpath = ''
 
 
 local checktl = function()
   package.path = package.path..';'..tluvit.tlpath
-  local exist,tl = pcall(lua_require,'tl')
+  local exist,tl = pcall(require,'tl')
 
   if not exist then
     print '\27[1;31mCannot find "tl" module inside package.path!!\n'
@@ -46,26 +46,26 @@ end
 <s>
 Load or require a .tl file. If the file returns something, this function also returnthe returned value in the .tl file.
 ]]
----@param path string? The path to .tl, defaults to "./maintl(.tl)"
+---@param path string? The path to .tl, defaults to "./maintl.tl"
 ---@return any module The return of the .tl
-tluvit.loadtl = function(path)
-  path = path or 'maintl'
+tluvit.loadtl = function(path,mode)
+  path = path or './maintl.tl'
+  mode = mode or 'ct'
+
+  if not fs.existsSync(path) then
+    error('Path to the .tl is not exist!')
+  end
 
   local tl = checktl()
-  tl.loader()
 
-  package.path = './deps/?.lua;./deps/?/init.lua;./?/init.lua;./?/init.tl;./deps/?.tl;./deps/?/init.tl;'..package.path
+  local tlfile = fs.readFileSync(path)
+  local mdl,err = tl.load(tlfile,path,mode)
 
-  local ok,mdl = pcall(lua_require,path)
-
-  if not ok and mdl then
-    print '\27[1;31mAn error has occurred while running the .tl!\n'
-    print 'The error:\27[0m'
-    print('\27[37;41m'..err..'\27[0m')
-    os.exit(1)
-  elseif ok and mdl then
-    return mdl
+  if not mdl and err then
+    error('\n\27[31;1mAn error has occured while running '..path..'\n\n'..err)
   end
+
+  return type(mdl) == "function" and mdl() or mdl
 end
 
 return tluvit
